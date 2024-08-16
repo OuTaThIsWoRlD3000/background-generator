99904297a6ca397a0433f60264b286c80b9e6d0c
OuTaThIsWoRlD3000 <ezanagetachew01@gmail.com>
2024-08-17 00:09:16 +0300

    added a extension

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Function to change the header content
-function changeHeader() {
-    document.getElementById("dynamicHeader").textContent = "A good worker";
-}
+var _ = require('lodash');
 
-// Add event listener to the button after the DOM content is loaded
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("changeHeaderButton").addEventListener("click", changeHeader);
-});
+console.log(_);
+let array = [1,2,3,4,5,6,7,8,9,10]
+console.log(_.without(array, 3))
+const header = document.getElementById("dynamicHeader");
+    document.getElementById("changeHeaderButton").addEventListener("click", () => {
+        header.textContent = "A good Teacher"  
+    });
+    document.getElementById("changeheaderbuttonagain").addEventListener("click", () => {
+        header.textContent = "ANDREI"  
+    })
